797d9ddf662b3b7b421c54108d2d34e218686dcc
BebertDeChast <74964221+BebertDeChast@users.noreply.github.com>
2021-03-16 12:16:09 +0100

    On branch partie_2.4 Changes to be committed: modified: test_dijkstra_opti.py
    adjusting minus things

--- test/test_dijkstra_opti.py
+++ test/test_dijkstra_opti.py
@@ -81,14 +81,14 @@ def dijkstra_opti_tests_2(number_of_node: int) -> None:
         start = time.time()
         tested_graph.dijkstra_basic_version(chosen_vertex)
         end = time.time()
-        time_dijkstra_basic += end - start
+        time_dijkstra_basic = end - start
         time_dijkstra_basic_array.append(time_dijkstra_basic)
 
         # For heap version of dijkstra :
         start = time.time()
         tested_graph.dijkstra_heap_version(chosen_vertex)
         end = time.time()
-        time_dijkstra_heap += end - start
+        time_dijkstra_heap = end - start
         time_dijkstra_heap_array.append(time_dijkstra_heap)
 
         # ploting
@@ -128,14 +128,14 @@ def dijkstra_opti_tests_3(number_of_node: int) -> None:
         start = time.time()
         tested_graph.dijkstra_basic_version(chosen_vertex)
         end = time.time()
-        time_dijkstra_basic += end - start
+        time_dijkstra_basic = end - start
         time_dijkstra_basic_array.append(time_dijkstra_basic)
 
         # For heap version of dijkstra :
         start = time.time()
         tested_graph.dijkstra_heap_version(chosen_vertex)
         end = time.time()
-        time_dijkstra_heap += end - start
+        time_dijkstra_heap = end - start
         time_dijkstra_heap_array.append(time_dijkstra_heap)
 
         # ploting
